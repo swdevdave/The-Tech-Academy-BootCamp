709a616a7fcc99015146eb1ee8d0843e1eddc4fd
Dave King <swdev.dave@gmail.com>
2017-03-24 19:17:13 -0700

    updated date time script
    updated date time script

--- Python/Drill 2/Datetime drill.py
+++ Python/Drill 2/Datetime drill.py
@@ -1,3 +1,12 @@
+#   Date time Exercise
+#   Autohor: Dave King
+#   03/24/2016
+#   Python 3.6
+
+
+
+
+
 from datetime import datetime
 
 now = datetime.now()
@@ -6,24 +15,47 @@ mn = (now.minute)
 
 
 
-NYC = hr + 3
+NYC = int(hr + 3)
 
-London = hr + 7
+TOD1 = NYC %24
 
+if TOD1 >12:
+    NTA = "PM"
+
+if TOD1 <12:    
+    NTA = "AM"
+
+
+
+London = int(hr + 7)
+TOD2 = London %24
+
+
+if TOD2 <12:    
+    NTB = "AM"
+
+if TOD2 >12:
+    NTB = "PM"
+
+if hr <12:    
+    NTC = "AM"
+
+if hr >12:
+    NTC = "PM"
 
 
 if NYC  >21:
-    print ("NYC is Closed and is", NYC %12,"'O clock there")
+    print ("NYC is Closed and is", NYC %12, mn, NTA)
 else:
-    print ("NYC is Open and is", NYC %12,"'O clock there")
+    print ("NYC is Open and is", NYC %12, mn, NTA)
 
 if London > 21:
-    print ("London is Closed and is", London %12,"'O clock there")
+    print ("London is Closed and is", London %12, mn, NTB)
 else:
-    print ("London is Open and is", London %12,"'O clock there")
+    print ("London is Open and is", London %12, mn, NTB)
 
 
-print ("Current time in Portland Oregon is", hr %12,":", mn )
+print ("Current time in Portland Oregon is", hr %12, mn ,NTC )
 
 
 
