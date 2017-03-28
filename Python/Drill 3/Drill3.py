ceb7e6d605bda676cf99b2966b0209b980de73c8
Dave King <swdev.dave@gmail.com>
2017-03-28 14:22:12 -0700

    added drill 3

---
+++ Python/Drill 3/Drill3.py
@@ -0,0 +1,16 @@
+import shutil
+
+shutil.move('C:\\Users\\Dave-Anico\\Desktop\\A\\1.txt',
+            'C:\\Users\\Dave-Anico\\Desktop\\B\\')
+
+shutil.move('C:\\Users\\Dave-Anico\\Desktop\\A\\2.txt',
+            'C:\\Users\\Dave-Anico\\Desktop\\B\\')
+
+shutil.move('C:\\Users\\Dave-Anico\\Desktop\\A\\3.txt',
+            'C:\\Users\\Dave-Anico\\Desktop\\B\\')
+
+shutil.move('C:\\Users\\Dave-Anico\\Desktop\\A\\4.txt',
+            'C:\\Users\\Dave-Anico\\Desktop\\B\\')
+
+
+print ('All files moved')
